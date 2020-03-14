2a49ef5582cbe272b7887f16118ebb9ad8c02b27
Mason Cao <mason.cao@freckleiot.com>
2020-03-14 14:49:58 -0400

    added spotify auth on app mount

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MainScreen } from './lib/screens'
+import { getSpotifyToken } from '@/services/spotify';
 
-class App extends React.Component{
-  render() {
-    return (
-      <MainScreen/>
-    );
-  };
+const App = () => {
+  const [auth, setAuth] = useState('');
+  useEffect(() => {
+    getSpotifyToken(setAuth);
+  }, []);
+  return (
+    <MainScreen token={auth}/>
+  );
 }
 
 export default App;
