7adee1ab1bae54121a315cec40a7648fcfd00355
Mason Cao <mason.cao@freckleiot.com>
2020-04-11 13:45:10 -0400

    added base case for gameplay

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import TrackPlayer from 'react-native-track-player';
+import Service from '@/services/trackPlayer/service';
+
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => Service);
