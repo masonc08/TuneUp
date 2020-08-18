b127bbccf9a6be6272c7676d8beda9a9235facf1
Mason <mxmcao@gmail.com>
2020-08-18 19:11:46 -0400

    add button for return to categories after picking

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-export const ENV = 'http://ec2-18-222-174-61.us-east-2.compute.amazonaws.com:3000/';
+// export const ENV = 'http://ec2-18-222-174-61.us-east-2.compute.amazonaws.com:3000/';
+export const ENV = 'http://10.0.2.2:3000/';
